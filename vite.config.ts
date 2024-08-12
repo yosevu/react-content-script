d8c94bf151c01a433ba4ebc06296b5edf34b3732
Yosevu Kilonzo <yosevu@yosevu.com>
2024-08-11 22:10:34 -0400

    fix: manifest build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import { defineConfig, Plugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json';
 
-// https://vitejs.dev/config/
+const viteManifestHackIssue846: Plugin & { renderCrxManifest: (manifest: any, bundle: any) => void } = {
+  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
+  name: 'manifestHackIssue846',
+  apply: 'build', 
+  renderCrxManifest(_manifest, bundle) {
+      bundle['manifest.json'] = bundle['.vite/manifest.json']
+      bundle['manifest.json'].fileName = 'manifest.json'
+      delete bundle['.vite/manifest.json']
+  },
+}
+
 export default defineConfig({
   plugins: [
     react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-    // Build Chrome Extension
+    viteManifestHackIssue846,
     crx({ manifest }),
   ],
 });
