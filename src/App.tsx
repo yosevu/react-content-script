234d72ed86fa7c201a1d00e4da457c760f0e36b5
Yosevu Kilonzo <yosevu@yosevu.com>
2024-08-11 22:39:18 -0400

    fix: correct popup styles and adjust size

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Logo from "./Logo";
 
 function App() {
   return (
-    <div className="w-92 text-center">
-      <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-2xl text-white">
-        <Logo className="h-40 pointer-events-none animate-spin-slow" id="App-logo" title="React logo" />
+    <div className="w-[400px] h-[500px] text-center">
+      <header className="bg-gray-800 w-full h-full flex flex-col items-center justify-center text-2xl text-white">
+        <Logo className="h-[40vmin] pointer-events-none animate-spin-slow" id="App-logo" title="React logo" />
         <p>Hello, World!</p>
         <p>I'm a Chrome Extension Popup!</p>
       </header>
