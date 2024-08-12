2e204a7cee5ab2c71a25a29cbce56f0050fbb3fa
Yosevu Kilonzo <yosevu@yosevu.com>
2024-08-11 22:33:14 -0400

    chore: add custom spin-slow animation to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", 
-    "./content-script/src/**/*.{js,ts,jsx,tsx}" 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./content-script/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 20s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
